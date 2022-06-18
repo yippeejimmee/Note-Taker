986155f1a9d7b9d42bb68261e389808e8c541b77
yippeejimmee <77139684+yippeejimmee@users.noreply.github.com>
2022-06-17 19:26:18 -0700

    completed readme

---
+++ Develop/README.md
@@ -0,0 +1,42 @@
+# Note Taker App
+
+An application that utilizes fs, express, path, util, and uuid in order to create and store notes.
+
+## Table of contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Test](#test)
+- [Credits](#credits)
+- [License](#license)
+- [Contribute](#contribute)
+- [Questions](#questions)
+
+## Installation
+
+First, open up the terminal and run "npm i" then "npm install express fs util uuid path" if not already installed.
+
+## Usage
+
+First navigate to the app <a href="https://jimmy-takes-notes.herokuapp.com/">here </a>. Click on "Get started button". Type in name of note and body of note and click save to save the note to left hand side. Click on saved note title on left hand side to pull up the saved note.
+
+## Test
+
+If you would like to run some tests, please input the following command:
+There are not tests to run.
+
+## Credits
+
+I would like to thank My friends, family, and instructors at UCSD bootcamp. for their support during the development of this application.
+
+## License
+
+This project is licensed under None.
+
+## Questions
+
+If you have any questions, please direct them towards my email at Jimmyqv.tran@gmail.com.  
+ You can find more of my work at github.com/yippeejimmee.
+
+![alt text](./public/assets/jimmy-takes-notes.herokuapp.com_.png)
+![alt text](./public/assets/jimmy-takes-notes.herokuapp.com_notes.png)
--- Develop/server.js
+++ Develop/server.js
@@ -1,3 +1,4 @@
+//brings in all the dependences
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
@@ -6,38 +7,53 @@ const {
     v4: uuidv4
 } = require('uuid');
 
+//creates server object with express
 const app = express();
+
+//creates port and allows it to communicate with heroku
 const PORT = process.env.PORT || 3001;
 
+//utilizes express to parse through data
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 
+//sets the public folder to static and allows users to view files
 app.use(express.static("public"));
 
+//sends the index.html file to user as welcome page
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 })
 
+//sends the notes.html file to user to interact with page
 app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/notes.html"))
 })
 
+//sends the database json to user to view all the notes in json form
 app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, "/db/db.json"))
 })
 
+//takes user req posts to apo/notes
 app.post('/api/notes', (req, res) => {
+    //assigns the body of req (title and note) to userNote
     const userNote = req.body;
+    //gives userNote and randomly generated id
     userNote.id = uuidv4();
+    //reads the db.json file
     fs.readFile("./db/db.json", "utf8", (err, data) => {
+        //if there is an error, console.error it
         if (err) {
             console.error(err);
         } else {
+            //stores the parsed data from db.json into parsedNotes
             const parsedNotes = JSON.parse(data);
-            console.log(data);
+            //pushes the user's note into the parsedNotes array
             parsedNotes.push(userNote);
+            //overwrites the db.json file with new stringified array of notes (parsedNotes)
             fs.writeFile('./db/db.json',
                 JSON.stringify(parsedNotes), (secondErr) => {
                     if (secondErr) {
@@ -53,16 +69,18 @@ app.post('/api/notes', (req, res) => {
     })
 });
 
+//deletes selected saved note
 app.delete('/api/notes/:id', (req, res) => {
+    //reads db.json file
     fs.readFile("./db/db.json", "utf8", (err, data) => {
+        //stores parsed data from db.json into deleteData
         let deleteData = JSON.parse(data);
-        console.log("data to delete", deleteData);
+        //filters through each of the data inside the parsed data and returns everything that does not match the id of selected note and stores it into newData
         const newData = deleteData.filter(datum => datum.id != req.params.id);
         if (err) {
             console.error(err);
         } else {
-
-
+            //writes new db.json using the newData array which has deleted the specific id
             fs.writeFile('./db/db.json',
                 JSON.stringify(newData), (deleteErr) => {
                     if (deleteErr) {
@@ -77,6 +95,7 @@ app.delete('/api/notes/:id', (req, res) => {
     })
 });
 
+//catches all url that is not specificied above and returns them to main page
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, "/public/index.html"))
 })
